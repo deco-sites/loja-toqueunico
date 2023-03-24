1ae2585e550f1b5540943682246335a641bb3d2a
rafaeltj13 <rafael.damaciel@gmail.com>
2023-03-24 16:43:47 -0300

    test

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,181 +1,51 @@
-import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
-import Text from "$store/components/ui/Text.tsx";
-import Container from "$store/components/ui/Container.tsx";
-
-import Newsletter from "./Newsletter.tsx";
-import type { ComponentChildren } from "preact";
-
-export type IconItem = { icon: AvailableIcons };
-export type StringItem = {
-  label: string;
-  href: string;
-};
-
-export type Item = StringItem | IconItem;
-
-export type Section = {
-  label: string;
-  children: Item[];
-};
-
-const isIcon = (item: Item): item is IconItem =>
-  // deno-lint-ignore no-explicit-any
-  typeof (item as any)?.icon === "string";
-
-function SectionItem({ item }: { item: Item }) {
-  return (
-    <Text variant="caption" tone="default-inverse">
-      {isIcon(item)
-        ? (
-          <div class="border-default border-1 py-1.5 px-2.5">
-            <Icon
-              id={item.icon}
-              width={25}
-              height={20}
-              strokeWidth={0.01}
-            />
-          </div>
-        )
-        : (
-          <a href={item.href}>
-            {item.label}
-          </a>
-        )}
-    </Text>
-  );
-}
-
-function FooterContainer(
-  { children, class: _class = "" }: {
-    class?: string;
-    children: ComponentChildren;
-  },
-) {
-  return <div class={`py-6 px-4 sm:py-12 sm:px-0 ${_class}`}>{children}</div>;
-}
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 
 export interface Props {
-  sections?: Section[];
+  imgSrc: LiveImage;
 }
 
-function Footer({ sections = [] }: Props) {
+function Footer({ imgSrc }: Props) {
   return (
-    <footer class="w-full bg-footer flex flex-col divide-y-1 divide-default">
-      <div>
-        <Container class="w-full flex flex-col divide-y-1 divide-default">
-          <FooterContainer>
-            <Newsletter />
-          </FooterContainer>
-
-          <FooterContainer>
-            {/* Desktop view */}
-            <ul class="hidden sm:flex flex-row gap-20">
-              {sections.map((section) => (
-                <li>
-                  <div>
-                    <Text variant="heading-3" tone="default-inverse">
-                      {section.label}
-                    </Text>
-
-                    <ul
-                      class={`flex ${
-                        isIcon(section.children[0]) ? "flex-row" : "flex-col"
-                      } gap-2 pt-2`}
-                    >
-                      {section.children.map((item) => (
-                        <li>
-                          <SectionItem item={item} />
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </li>
-              ))}
-            </ul>
-
-            {/* Mobile view */}
-            <ul class="flex flex-col sm:hidden sm:flex-row gap-4">
-              {sections.map((section) => (
-                <li>
-                  <Text variant="body" tone="default-inverse">
-                    <details>
-                      <summary>
-                        {section.label}
-                      </summary>
-
-                      <ul
-                        class={`flex ${
-                          isIcon(section.children[0]) ? "flex-row" : "flex-col"
-                        } gap-2 px-2 pt-2`}
-                      >
-                        {section.children.map((item) => (
-                          <li>
-                            <SectionItem item={item} />
-                          </li>
-                        ))}
-                      </ul>
-                    </details>
-                  </Text>
-                </li>
-              ))}
-            </ul>
-          </FooterContainer>
-        </Container>
+    <footer class="w-full bg-[#242D21]">
+      <div class="hidden md:block  w-full">
+        <div class="flex justify-between items-center px-[118px] py-[64px] text-[#ECCDA5] text-[24px] font-normal">
+          <a
+            href="/"
+            aria-label="home"
+            className="cursor-pointer"
+          >
+            <img
+              class="object-cover w-[215px]"
+              src={imgSrc}
+              alt="logo"
+              loading="eager"
+            />
+          </a>
+          <p class="cursor-pointer hover:text-white">Termos e condições</p>
+          <p class="cursor-pointer hover:text-white">Privacidade</p>
+          <p class="cursor-pointer hover:text-white">CNPJ + endereço</p>
+        </div>
       </div>
-
-      <div>
-        <Container class="w-full">
-          <FooterContainer class="flex justify-between w-full">
-            <Text
-              class="flex items-center gap-1"
-              variant="body"
-              tone="default-inverse"
-            >
-              Powered by{" "}
-              <a
-                href="https://www.deco.cx"
-                aria-label="powered by https://www.deco.cx"
-              >
-                <Icon id="Deco" height={20} width={60} strokeWidth={0.01} />
-              </a>
-            </Text>
-
-            <ul class="flex items-center justify-center gap-2">
-              <li>
-                <a
-                  href="https://www.instagram.com/deco.cx"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Instagram logo"
-                >
-                  <Icon
-                    class="text-default-inverse"
-                    width={32}
-                    height={32}
-                    id="Instagram"
-                    strokeWidth={1}
-                  />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="http://www.deco.cx/discord"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Discord logo"
-                >
-                  <Icon
-                    class="text-default-inverse"
-                    width={32}
-                    height={32}
-                    id="Discord"
-                    strokeWidth={5}
-                  />
-                </a>
-              </li>
-            </ul>
-          </FooterContainer>
-        </Container>
+      <div class="md:hidden pt-[24px] pb-[42px] px-[16px] flex flex-col text-[#ECCDA5] text-[11px] font-normal items-center justify-center">
+        <p class="pb-[32px]">
+          <a
+            href="/"
+            aria-label="home"
+            className="cursor-pointer"
+          >
+            <img
+              class="object-cover w-[170px]"
+              src={imgSrc}
+              alt="logo"
+              loading="eager"
+            />
+          </a>
+        </p>
+        <div class="flex items-center justify-between w-full">
+          <p class="cursor-pointer hover:text-white">Termos e condições</p>
+          <p class="cursor-pointer hover:text-white">Privacidade</p>
+          <p class="cursor-pointer hover:text-white">CNPJ + endereço</p>
+        </div>
       </div>
     </footer>
   );
---
+++ sections/GiveYourTouch.tsx
@@ -0,0 +1,24 @@
+import Button from "$store/components/ui/Button.tsx";
+
+export default function GiveYourTouch() {
+  return (
+    <div class=" bg-[#D9D9D9] flex flex-col items-center justify-center h-[221px] md:h-[374px] w-full">
+      <div class="w-[198px] md:w-[669px] flex flex-col items-center justify-center">
+        <p class="font-normal text-[11px] md:text-[24px] text-black text-center mb-3 md:mb-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+          consectetur lectus sed leo ultricies.
+        </p>
+        <div class="hidden md:inline">
+          <Button size="large" variant="primary">
+            DÊ O SEU TOQUE
+          </Button>
+        </div>
+        <div class="md:hidden inline">
+          <Button size="small" variant="primary">
+            DÊ O SEU TOQUE
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
